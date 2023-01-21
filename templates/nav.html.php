e7ff56101a93211057e5faedb0262791609ae533
trekpio2 <piotrmichalak2000@o2.pl>
2023-01-21 17:34:35 +0100

    delete unused files

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -4,10 +4,9 @@
 
 ?>
 <ul>
+    <li><a href="<?= $router->generatePath('map-index') ?>">Home</a></li>
     <?php foreach ($buildings as $building): ?>
             <li><a href="<?= $router->generatePath('map-show', ['id' => $building->getId()]) ?>"> <?= $building->getName() ?> </a></li>
     <?php endforeach; ?>
-    <li><a href="<?= $router->generatePath('map-index') ?>">Home</a></li>
-    <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
 </ul>
 <?php
